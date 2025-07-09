bfe7fb6b86461dd1b4c72199e58f5a38326d5024
xiokevc <xiokevc@gmail.com>
2025-07-09 11:55:12 -0400

    update app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const storyNodes = [
+    {
+        id: 'start'
+
+    }
+
+];
+
+const msg = ['Game Over', 'You have Escaped!'];
+
+const menu = ['Exit Game', 'Resume', 'Restart']
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let currentNodeId;
+let isGamerOver = false;
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const messageEl = document.getElementById('');
+
+/*-------------------------------- Functions --------------------------------*/
+
+function init() {
+
+};
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+/*--------------------------- Start the game! ---------------------------*/
+
+init();
+
