2cc7cc4f9765f6709612aba32754742faea92122
xiokevc <xiokevc@gmail.com>
2025-07-10 16:54:57 -0400

    added audio

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
 /*-------------------------------- Constants --------------------------------*/
-
-// const button = document.getElementById('button');
+const = document.getElementById('');
+let = false;
 
 /*---------------------------- Variables (state) ----------------------------*/
 
 
-
 /*------------------------ Cached Element References ------------------------*/
-
 
 /*-------------------------------- Functions --------------------------------*/
 
 
-
 /*----------------------------- Event Listeners -----------------------------*/
-
-// exploreBtn.addEventListener('click', () => {
-//   option2Page.classList.add('hidden');
-//   exploreSection.classList.remove('hidden');
-// });
-
-// runBtn.addEventListener('click', () => {
-//   alert("You ran"); // Replace
-// });
 
 /*--------------------------- Start the game! ---------------------------*/
 
